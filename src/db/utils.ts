d69ccb99048e7af521164ce49eb05422ec933302
vargroozzz <edand2002@gmail.com>
2021-04-18 00:29:39 +0300

    moved from local users.json file to postgres DB on Heroku

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,31 +1,59 @@
-import fs from "fs/promises";
-import {pipe} from "fp-ts/function";
-import * as O from "fp-ts/Option";
-import {elem, findFirst, map} from "fp-ts/Array";
-import * as N from "fp-ts/number";
-import {watchFile} from "fs";
-import {User} from "./types";
-import {flow} from "fp-ts/lib/function";
+import fs from "fs/promises"
+
+import * as N from "fp-ts/lib/number"
+import * as Eq from "fp-ts/lib/Eq"
+import {pipe, flow} from "fp-ts/lib/function"
+import * as O from "fp-ts/lib/Option"
+import {elem, findFirst, map} from "fp-ts/lib/Array"
+import pkg from 'pg'
+const { Pool } = pkg
+
+import {User} from "./types"
+import {requestProfile} from "../amqp/trading"
+import {publish} from "../amqp"
 
 const usersFilePath = "./users.json"
 const maintenancesFilePath = "./maintenances.json"
 const offersFilePath = "./offers.json"
 
-const readUsers = () => fs.readFile(usersFilePath, "utf-8")
+const db = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+}
+
+const pool = new Pool(db)
+
+// const readUsers = () => fs.readFile(usersFilePath, "utf-8")
 export const readMaintenances = () => fs.readFile(maintenancesFilePath, "utf-8")
 export const readOffers = () => fs.readFile(offersFilePath, "utf-8")
 
-const writeUsers = (users: string) => fs.writeFile(usersFilePath, users)
+// const writeUsers = (users: string) => fs.writeFile(usersFilePath, users)
 
-export const getUsers = async (): Promise<User[]> => {
-    const users = await readUsers()
-    return JSON.parse(users)
-}
+// export const getUsers = async (): Promise<User[]> => {
+//     const users = await readUsers()
+//     return JSON.parse(users)
+// }
 
-const setUsers: (users: User[]) => Promise<void> = flow(
-    JSON.stringify,
-    writeUsers
-)
+const makeQuery = (query: string) => pool.query(query)
+
+export const getUsers = (): Promise<User[]> => pool.query("SELECT bot_user FROM bot_users").then(res => typeof res.rows[0] === "string" ? map(JSON.parse)(res.rows) : res.rows).then(map(user => user.bot_user))
+export const insertNewUser = (user: User) => pool.query(`INSERT INTO bot_users (bot_user) VALUES ('${JSON.stringify(user)}')`).then(console.log)
+
+// const setUsers: (users: User[]) => Promise<void> = flow(
+//     JSON.stringify,
+//     writeUsers
+// )
+
+// const setUsers: (users: User[]) => Promise<void> = flow(
+//     map(user => `('${JSON.stringify(user)}')`),
+//     users => users.join(','),
+//     users => `INSERT INTO users (user) VALUES ${users}`,
+//     query => pool.query(query),
+//     res => new Promise<void>(resolve => null)
+//
+// )
 
 export const createUser = (id: number): User => ({
     id: id,
@@ -43,20 +71,26 @@ export const createUser = (id: number): User => ({
 })
 
 
-export const updateUsers = async (fn: (arg0: User[]) => User[]) => {
-    const users = await getUsers()
-    return pipe(
-        users,
-        fn,
-        setUsers,
-    )
-}
+// export const updateUsers = async (fn: (arg0: User[]) => User[]) => {
+//     const users = await getUsers()
+//     return pipe(
+//         users,
+//         fn,
+//         setUsers,
+//     )
+// }
 export const mapUser = (fn: (arg0: User) => User, id: number) => map((user: User) => user.id === id ? fn(user) : user)
+// export const modifyUser = flow(
+//     mapUser,
+//     updateUsers,
+// )
 export const getUserById = (id: number) => findFirst((user: User) => user.id === id)
-export const updateUserById = flow(
-    mapUser,
-    updateUsers,
-)
+// export const getUserById = (id: number): Promise<User> => pool.query(`SELECT bot_user FROM bot_users WHERE bot_user ->> 'id' = '${id}'`).then(res => res.rows[0])
+export const selectUserById = (id: number) => getUsers().then(getUserById(id))
+export const updateUserById = (fn: (arg0: User) => User) => (id: number) => selectUserById(id).then(flow(
+    O.map(fn),
+    O.map(user => pool.query(`UPDATE bot_users SET bot_user = bot_user || '${JSON.stringify(user)}'::jsonb WHERE bot_user ->> 'id' = '${id}'`).then(console.log),)
+))
 
 export const getOffersList = async (id: number): Promise<O.Option<[string, string][]>> => {
     const users = await getUsers()
@@ -67,17 +101,18 @@ export const getOffersList = async (id: number): Promise<O.Option<[string, strin
     )
 }
 
-export const setOffersList = (id: number, offersList: readonly [string, string][]) =>
-    updateUsers(mapUser(user => ({...user, offersList: [...offersList]}), id))
+export const setOffersList = (offersList: readonly [string, string][]) => updateUserById(user => ({...user, offersList: [...offersList]}))
+    // updateUsers(mapUser(user => ({...user, offersList: [...offersList]}), id))
 
 export const addUser = flow(
     createUser,
-    user => updateUsers(users => [...users, user]),
+    insertNewUser,
 )
 
 
-export const setToken = async (id: number, token: string) =>
-    updateUserById(user => ({...user, token: token, rights: {...user.rights, basic: true, profile: true, trade: true}}), id)
+export const setToken = (token: string) =>
+    updateUserById(user => ({...user, token: token, rights: {...user.rights, basic: true, profile: true, trade: true}}))
+
 export const getToken = async (id: number) => {
     const maybeUser = await selectUserById(id)
     return pipe(
