29dd9c50ec2f6ca72f6f5f6dee5553494df97e98
vargroozzz <edand2002@gmail.com>
2021-04-18 00:28:06 +0300

    moved bot to webhooks

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import runKafka from "./kafka/index.js"
 import bot from "./bot";
 import {runAMQPConsumer} from "./amqp";
 
+const TOKEN = process.env.BOT_TOKEN
+const HOST_URL = process.env.HOST_URL
+const PORT = parseInt(process.env.PORT ?? "")
+if (isNaN(PORT)) throw new Error("Port is required")
+
 runKafka()
 runAMQPConsumer()
-bot.launch()
+bot.launch({
+    webhook: {
+        domain: HOST_URL,
+        port: PORT,
+        // tlsOptions: null,
+        hookPath: `/bot${TOKEN}`
+    }
+});
